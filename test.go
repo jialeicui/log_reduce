73ba14d74d09d08f3719ae8477b8156456102342
unknown <jialeicui@126.com>
2014-12-04 22:57:43 +0800

    ip part done

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -16,21 +17,39 @@ func deal_time(lhs, rhs string) int64 {
 	return t.Unix()
 }
 
+func inet_aton(ipnr net.IP) int {
+	bits := strings.Split(ipnr.String(), ".")
+
+	b0, _ := strconv.Atoi(bits[0])
+	b1, _ := strconv.Atoi(bits[1])
+	b2, _ := strconv.Atoi(bits[2])
+	b3, _ := strconv.Atoi(bits[3])
+
+	var sum int
+
+	sum += int(b0) << 24
+	sum += int(b1) << 16
+	sum += int(b2) << 8
+	sum += int(b3)
+
+	return sum
+}
+
 func deal_ip_port(ip_port string) (int, int) {
 	ip_port = strings.Trim(ip_port, ":")
 	ip_port = strings.Replace(ip_port, "#", ":", 1)
-	host, port, _ := net.SplitHostPort(ip_port)
-	ip := net.ParseIP(host)
-	fmt.Print(ip)
-	_ = port
-	return 1, 2
+	host, port_str, _ := net.SplitHostPort(ip_port)
+	ip_o := net.ParseIP(host)
+
+	port, _ := strconv.Atoi(port_str)
+	return inet_aton(ip_o), port
 }
 
 func main() {
 	line := "[2014-12-04 13:27:54.758 LEVEL = QUERY] client 61.220.4.123#63013: view chunghwa-telecom: query:int.dpool.sina.com.cn. IN 1"
 	depart := strings.Split(line, " ")
 	t := deal_time(depart[0], depart[1])
-	a, _ := deal_ip_port(depart[6])
+	a, p := deal_ip_port(depart[6])
 	_ = t
-	_ = a
+	fmt.Print(a, p)
 }
